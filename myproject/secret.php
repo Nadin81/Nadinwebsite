ce99ec36b64f4118f104f28fefd40103adddd840
Nadin81 <nadinalac2015@yahoo.com>
2025-01-19 07:08:16 +0100

    Add files via upload

---
+++ myproject/secret.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitel = "Home";
+require_once "includes/header.php";
+require_once "includes/nav.php";
+?>
+ <div class="secret-container">
+        <h1>Geheime Informationen</h1>
+        <p>Dies ist eine streng vertrauliche Seite, die nur für autorisierte Personen zugänglich ist.</p>
+
+        <h2>Private Daten</h2>
+        <p><strong>Name:</strong> Nadin Aldik</p>
+        <p><strong>Adresse:</strong> Steinfurtweg 77 ,44345 Dortmund</p>
+        <p><strong>Telefonnummer:</strong>03456334567</p>
+        
+
+        <p class="warning">Warnung: Geben Sie diese Informationen nicht weiter.</p>
+
+        <div class="footer">&copy; 2025 Geheime Seite. Alle Rechte vorbehalten.</div>
+    </div>
+    <?php
+require_once "includes/footer.php";
+?>
